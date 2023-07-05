b0bc213bec20b5fdbf9d20e5474468e613854e87
Anzhe Meng <meng@prepbaseballreport.com>
2023-07-05 00:00:29 -0400

    added data-stream as env

--- lambda/service/config.py
+++ lambda/service/config.py
@@ -7,7 +7,8 @@ def load_env():
     """Load enviroment variables."""
     try:
         return {
-                "DATA_STREAM": os.environ['DATA_STREAM']
+                "DATA_STREAM": os.environ['DATA_STREAM'], 
+                "LOGGING_LEVEL": os.environ['LOGGING_LEVEL']
                 }
     except KeyError as error:
         LOGGER.exception("Enviroment variable %s is required.", error)
