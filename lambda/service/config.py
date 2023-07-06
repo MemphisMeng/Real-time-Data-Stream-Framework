0e47aaa967c31613e808dfeaeeec7e3099f4cd06
Anzhe Meng <meng@prepbaseballreport.com>
2023-07-05 22:43:15 -0400

    blacked

--- lambda/service/config.py
+++ lambda/service/config.py
@@ -7,9 +7,9 @@ def load_env():
     """Load enviroment variables."""
     try:
         return {
-                "DATA_STREAM": os.environ['DATA_STREAM'], 
-                "LOGGING_LEVEL": os.environ['LOGGING_LEVEL']
-                }
+            "DATA_STREAM": os.environ["DATA_STREAM"],
+            "LOGGING_LEVEL": os.environ["LOGGING_LEVEL"],
+        }
     except KeyError as error:
         LOGGER.exception("Enviroment variable %s is required.", error)
         sys.exit(1)
--- lambda/service/service.py
+++ lambda/service/service.py
@@ -3,19 +3,24 @@ import hashlib, sys, json, boto3, logging
 
 LOGGER = logging.getLogger(__name__)
 
+
 def main(event, environment):
 
     LOGGER.info(event)
 
-    data_stream = environment['DATA_STREAM']
+    data_stream = environment["DATA_STREAM"]
     try:
         output = []
-        for record in event['Records']:
-            if record['eventName'] in ['INSERT', 'MODIFY']:
-                converted_record = unmarshal_dynamodb_json(record['dynamodb']['NewImage'])
-                converted_record['streaming_timestamp'] = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f")
+        for record in event["Records"]:
+            if record["eventName"] in ["INSERT", "MODIFY"]:
+                converted_record = unmarshal_dynamodb_json(
+                    record["dynamodb"]["NewImage"]
+                )
+                converted_record["streaming_timestamp"] = datetime.utcnow().strftime(
+                    "%Y-%m-%d %H:%M:%S.%f"
+                )
                 output.append(converted_record)
-            elif record['eventName'] == 'REMOVE':
+            elif record["eventName"] == "REMOVE":
                 pass
 
         if len(output) > 0:
@@ -23,15 +28,16 @@ def main(event, environment):
             LOGGER.info(f"this is output: {output}")
             put_batch_data_stream(output, partition_key, data_stream)
 
-
     except Exception as e:
         LOGGER.error(str(e), exc_info=True)
         sys.exit(1)
 
+
 def unmarshal_dynamodb_json(node):
     data = dict({})
-    data['M'] = node
+    data["M"] = node
     return unmarshal_value(data)
+
 
 def unmarshal_value(node):
     if type(node) is not dict:
@@ -49,21 +55,21 @@ def unmarshal_value(node):
         # NN – Number Set - not handled
         # BB – Binary Set - not handled
         key = key.lower()
-        if key == 'bool':
+        if key == "bool":
             return value
-        if key == 'null':
+        if key == "null":
             return None
-        if key == 's':
+        if key == "s":
             return value
-        if key == 'n':
-            if '.' in str(value):
+        if key == "n":
+            if "." in str(value):
                 return float(value)
             return int(value)
-        if key in ['m', 'l']:
-            if key == 'm':
+        if key in ["m", "l"]:
+            if key == "m":
                 data = {}
                 for key1, value1 in value.items():
-                    if key1.lower() == 'l':
+                    if key1.lower() == "l":
                         data = [unmarshal_value(n) for n in value1]
                     else:
                         if type(value1) is not dict:
@@ -74,28 +80,20 @@ def unmarshal_value(node):
             for item in value:
                 data.append(unmarshal_value(item))
             return data
-        
+
+
 def put_batch_data_stream(output, partition_key, data_stream_name):
 
-    client = boto3.client('kinesis')
+    client = boto3.client("kinesis")
     records = []
     count = 1
     for observation in output:
         if count % 20 == 0:
-            client.put_records(
-                StreamName=data_stream_name,
-                Records= records
-            )
+            client.put_records(StreamName=data_stream_name, Records=records)
             records.clear()
-        record = {
-            "Data": json.dumps(observation) + '\n',
-             'PartitionKey': partition_key
-        }
+        record = {"Data": json.dumps(observation) + "\n", "PartitionKey": partition_key}
         records.append(record)
         count = count + 1
 
     if len(records) > 0:
-        client.put_records(
-                StreamName=data_stream_name,
-                Records= records
-            )
+        client.put_records(StreamName=data_stream_name, Records=records)
