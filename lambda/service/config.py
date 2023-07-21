88f8383fe84b63afe6555c796a8a78422b5c35c7
Anzhe Meng <meng@prepbaseballreport.com>
2023-07-21 18:02:43 -0400

    added docstrings

--- lambda/service/config.py
+++ lambda/service/config.py
@@ -4,7 +4,21 @@ LOGGER = logging.getLogger(__name__)
 
 
 def load_env():
-    """Load enviroment variables."""
+    """Load environment variables.
+
+    Returns:
+       dict: A dictionary containing the loaded environment variables.
+
+    Raises:
+        KeyError: If any required environment variable is missing.
+
+    Notes:
+        - The function attempts to load several environment variables including:
+            - LOGGING_LEVEL: Specifies the logging level for the application.
+            - STREAM: The name of the outputing kinesis delivery stream
+        - If any of the required environment variables are missing, a KeyError is raised.
+        - The function logs an exception message indicating the missing environment variable and exits the program with a status code of 1.
+    """
     try:
         return {
             "STREAM": os.environ["STREAM"],
